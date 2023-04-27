af25b27543c516153ab38d54fc2c29682b59e3c3
Saul <drsaul1984@gmail.com>
2023-04-27 09:59:31 -0700

    Agregada prueba para dia lunes.

---
+++ GalgosApp/MateriasClase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalgosApp
+{
+    internal class MateriasClase
+    {
+        public string Nombre { get; set; }
+        public string Horario { get; set; }
+        public string Salon { get; set; }
+    }
+}
